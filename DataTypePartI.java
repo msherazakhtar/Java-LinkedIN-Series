0c0d8b4d4f1e4782d2f7ee5ae3154842a72bb6ed
sheraz <sherazakhtarmalik@gmail.com>
2025-08-25 01:55:00 +0500

    Java LinkedIn Series

---
+++ DataTypePartI.java
@@ -0,0 +1,16 @@
+package javaLinkedInSeries;
+
+public class DataTypePartI {
+    public static void main(String[] args) {
+        int age = 25;
+        float pi = 3.14f;
+        double salary = 55000.99;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        System.out.println("Age: " + age);
+        System.out.println("Value of Pi: " + pi);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Fun? " + isJavaFun);
+    }
+}
---
+++ HellowWorld.java
@@ -0,0 +1,9 @@
+package javaLinkedInSeries;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello LinkedIn World");
+	}
+
+}
